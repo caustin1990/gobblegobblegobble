09bb9675065a29acf81a9411272e4c89225742a1
Corey Austin <caustin1990@gmail.com>
2017-02-14 15:50:18 -0500

    Known Working
    The Sikuli code isn't being called properly in this submission. The next
    bit shall focus on inserting the known working Sikuli code into this
    project.

--- Simple Test/simplesikulitest.sikuli/simplesikulitest.py
+++ Simple Test/simplesikulitest.sikuli/simplesikulitest.py
@@ -1,4 +1,6 @@
 find("1484944773662.png")
 time.sleep(3)
-click("1484944780578.png")
-time.sleep(5)
+numbers = [3,4,5,6]
+for item in numbers:
+	click("1484944780578.png")
+	time.sleep(3)
--- simple_test.sikuli/simple_test.py
+++ simple_test.sikuli/simple_test.py
@@ -1,18 +1,36 @@
 import os 
 import re 
 import csv
+import time
 
 setOfFunctions = {}
-
-
+"""
+Global Variables
+"""
+base_dir = 	'C:\Users\\f002nx3\Documents\GitHub\gobblegobblegobble\simple_test.sikuli'
+current_test_dir = ''
 def file_len(fname):
     with open(fname) as f:
         for i, l in enumerate(f):
             pass
     return i + 1
+	
+def prependImageURL(inImg):
+	inImg = current_test_dir + '\\target_images\\' + inImg
+	return inImg
 
 def search_image(img1, img2, waitTime):
-	print "search image"
+	img1_path = prependImageURL(img1)
+	img2_path = prependImageURL(img2)
+	print "in search image function"
+	if(img1): print "image one exists" + img1
+	if(waitTime): print "this is your wait time" + waitTime
+	if(img2): print "image two exists" + img2	
+	try:
+		#find(img1_path)
+		find(img1_path, waitTime)
+	except FindFailed:
+		print "there's something happening here"
 	#if(img2):
 		#find(img1)
 	#	wait(waitTime)
@@ -24,8 +42,13 @@ def wait(inWait):
 	print inWait + ' wait'	
 
 
-def click(inClick):
-	print inClick + ' click'
+def click(inClickImage):
+	inClickImage = prependImageURL(inClickImage)
+	print 'we are totally clicking this image'
+	
+	click(inClickImage)
+	print 'we must have totally clicked this image'
+	print inClickImage + ' click'
 
 
 def typekeys(inKey):
@@ -36,8 +59,10 @@ def typekeycombo(inKeyCombo):
 	print inKeyCombo + ' type key combo'
 
 
-def sleep(inSleep):
-	print inSleep + ' sleep'
+def sikuli_sleep(inSleep):
+	time.sleep(int(inSleep))
+	print 'this is that sleep thang you were thinking about'
+	#time.sleep(inSleep)
 
 def checksuccess(inSuccessMessage):
 	print inSuccessMessage + ' success!'
@@ -45,23 +70,22 @@ def checksuccess(inSuccessMessage):
 def run_function(inFunction, inValue):
 	#the following functions need to be made here: 
 	#TypeKeys, TypeKeyCombo, Sleep, CheckSuccess
-	print "run_function called|arguement passed|inFunction"+str(inFunction)
-	print "run_function called|arguement passed|inFunction"+str(inValue)
+	print "run_function | function called -> "+str(inFunction)
+	print "run_function | value passed in -> "+str(inValue)
 	if(inFunction == 'typekeys'):
 		print "calling typekeys function"
 		typekeys(inValue)
 	elif(inFunction == 'sleep'):
 		print "calling sleep function"
-		sleep(inValue)
+		print "VALUE | " + inValue
+		print "TYPE  | " + inValue
+		sikuli_sleep(inValue)
 	elif(inFunction == 'typekeycombo'):
 		print "calling typkeycombo function"
 		typekeycombo(inValue)
 	elif(inFunction == 'wait'):
 		print "calling wait function"
 		wait(inValue)
-	elif(inFunction == 'sleep'):
-		print "calling sleep function"
-		sleep(inValue)
 	elif(inFunction == 'checksuccess'):
 		print "calling checksuccess function"
 		checksuccess(inValue)
@@ -72,8 +96,9 @@ def run_function(inFunction, inValue):
 def run_test_step(funcName, funcVal, logEntry, errorEntry):
 	#this is where we start using sikuli code
 	funcName = funcName.lower()
-	#make log entry here
-	#make error log entry here too
+	#TODO:make log entry here
+	#TODO:make error log entry here too
+	#TOTO:make error handling try catch statement
 	run_function(funcName, funcVal)
 
 
@@ -95,21 +120,25 @@ def run_test(instructions_from_file):
 			search_image(image1,image2,waitTime)
 		print "calling run_test_step"
 		run_test_step(functionToCall,args,comment,failure)
-		
-tests_dir = "/Users\/f002nx3/Documents/GitHub/gobblegobblegobble/windows_application_test_tool.sikuli\/application_tests/"
 
+		
+	
+
+tests_dir = base_dir + '\\application_tests'
 #find each testfile.csv and execute 
 for test_folder in os.listdir(tests_dir):	
 
 	test_file = test_folder + ".csv"
-	test_dir = tests_dir + test_folder
-	test_file = test_dir + "/" + test_file
-	
+	test_dir = tests_dir + "\\" + test_folder
+	global current_test_dir 
+	current_test_dir 	= test_dir
+	test_file = test_dir + "\\" + test_file
+	print test_file
+	print str(os.path.isdir(test_dir))
 	#find test files in test folder exclude OSX's .DS_Store file
 	if(os.path.isdir(test_dir) and not re.match("\.DS_Store", test_dir)): 
 		#the test folder and file have the same name
 		#get test parameters
-		
 		try:
 			instructions = []
 			with open(test_file, "r") as file:
